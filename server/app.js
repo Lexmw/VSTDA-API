22eb142fef0ac401b3966c8e9152f3571720310c
lexius <lexius.waltar@gmail.com>
2019-07-11 17:46:46 -0700

    complete the api

--- server/app.js
+++ server/app.js
@@ -4,28 +4,28 @@ const morgan = require('morgan');
 const PORT = 8484;
 const app = express();
 
-const data = [
-    {
-      todoItemId: 0,
-      name: 'an item',
-      priority: 3,
-      completed: false
-    },
-    {
-      todoItemId: 1,
-      name: 'another item',
-      priority: 2,
-      completed: false
-    },
-    {
-      todoItemId: 2,
-      name: 'a done item',
-      priority: 1,
-      completed: true
-    }
-];
+app.use(express.json());
 
-// add your code here
+const data = [
+  {
+    todoItemId: 0,
+    name: 'an item',
+    priority: 3,
+    completed: false
+  },
+  {
+    todoItemId: 1,
+    name: 'another item',
+    priority: 2,
+    completed: false
+  },
+  {
+    todoItemId: 2,
+    name: 'a done item',
+    priority: 1,
+    completed: true
+  }
+];
 
 app.get('/', (req, res) => {
     res.send({status:'ok'}).status(200);
@@ -35,18 +35,37 @@ app.get('/api/TodoItems', (req, res) =>{
     res.send(data);
 });
 
-app.get('/api/TodoItems/:1', (req, res) => {
-  console.log(req.param.number);
-  if(req.param.number === 1){
-    res.send(data.map(name));
-    console.log(data.map(name));
-  }    
+app.get('/api/TodoItems/:id', (req, res) => {
+  
+  for(let i = 0; i < data.length; i++) {
+    if(data[i]['todoItemId'] == req.params.id) {
+        res.send(data[i]);
+    }
+}
 });
 
+//Add an item to the dataset. If there is already an item with a matching todoItemId, overwrite the existing item.
+app.post('/api/TodoItems', (req, res) => {
+  for(let i = 0; i < data.length; i++) {
+    if(req.body.todoItemId === data[i].todoItemId) {
+        data[i] = req.body;
+    } else {
+        data.push(req.body);
+    };
+};
+res.status(201).send(req.body);
+});
 
-// app.post('/api/TodoItems/:number', (req, res) =>{
-
-// });
-
+app.delete('/api/TodoItems/:id', (req,res) => {
+  var newData = [];
+	// check if ids are the same, delete from mockData
+	for(var i = 0; i < data.length; i++) {
+		if(data[i]['todoItemId'] == req.params.id) {
+			newData = data.splice(i, 1);
+			// set status and send back copy of deleted item
+			res.status(200).send(newData[i]);
+		}
+	}
+});
 
 module.exports = app;
